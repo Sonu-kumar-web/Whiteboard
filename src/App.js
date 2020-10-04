15fd4ad69e23c838b74f0c5dd39569f508f85823
Sonu <sonu3660@gmail.com>
2020-10-04 13:44:21 +0530

    Added logic for line and rectangle

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Rough from "roughjs/bundled/rough.esm";
 
 const generator = Rough.generator();
 
-function createElement(x1, y1, x2, y2) {
-   const roughElement = generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+function createElement(x1, y1, x2, y2, type) {
+   const roughElement =
+      type === "line"
+         ? generator.line(x1, y1, x2, y2)
+         : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
    return { x1, y1, x2, y2, roughElement };
 }
 
 const App = () => {
    const [element, setElement] = useState([]);
    const [drawing, setDrawing] = useState(false);
+   const [elementType, setElementType] = useState("line");
 
    useLayoutEffect(() => {
       const canvas = document.getElementById("canvas");
@@ -22,15 +26,23 @@ const App = () => {
       element.forEach((ele) => roughCanvas.draw(ele.roughElement));
    }, [element]);
 
+   // When mouse move Down
    const handleMouseDown = (e) => {
       setDrawing(true);
 
       const { clientX, clientY } = e;
 
-      const element = createElement(clientX, clientY, clientX, clientY);
+      const element = createElement(
+         clientX,
+         clientY,
+         clientX,
+         clientY,
+         elementType
+      );
       setElement((prevState) => [...prevState, element]);
    };
 
+   // When mouse Move
    const handleMouseMove = (e) => {
       if (!drawing) {
          return;
@@ -40,19 +52,42 @@ const App = () => {
       const index = element.length - 1;
       const { x1, y1 } = element[index];
 
-      const updateElement = createElement(x1, y1, clientX, clientY);
+      const updateElement = createElement(
+         x1,
+         y1,
+         clientX,
+         clientY,
+         elementType
+      );
 
       const elementCopy = [...element];
       elementCopy[index] = updateElement;
       setElement(elementCopy);
    };
 
+   // When mouse move UP
    const handleMouseUp = () => {
       setDrawing(false);
    };
 
    return (
       <div>
+         <div style={{ position: "fixed" }}>
+            <input
+               type="radio"
+               id="line"
+               checked={elementType === "line"}
+               onChange={() => setElementType("line")}
+            />
+            <label htmlFor="line">Line</label>
+            <input
+               type="radio"
+               id="rectangle"
+               checked={elementType === "rectangle"}
+               onChange={() => setElementType("rectangle")}
+            />
+            <label htmlFor="rectangle">Rectangle</label>
+         </div>
          <canvas
             id="canvas"
             style={{ background: "lightGray" }}
