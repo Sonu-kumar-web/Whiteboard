ce351e20a7b0d1d2ae90a1eac64b683208625506
Sonu <sonu3660@gmail.com>
2020-10-05 12:50:57 +0530

    Created eclipse and circle logics

--- src/App.js
+++ src/App.js
@@ -3,18 +3,63 @@ import Rough from "roughjs/bundled/rough.esm";
 
 const generator = Rough.generator();
 
-function createElement(x1, y1, x2, y2, type) {
-   const roughElement =
-      type === "line"
-         ? generator.line(x1, y1, x2, y2)
-         : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-   return { x1, y1, x2, y2, roughElement };
+function createElement(id, x1, y1, x2, y2, type) {
+   let roughElement;
+   if (type === "line") {
+      roughElement = generator.line(x1, y1, x2, y2);
+   } else if (type === "rectangle") {
+      roughElement = generator.rectangle(x1, y1, x2 - x1, y2 - y1);
+   } else if (type === "circle") {
+      roughElement = generator.circle(x1, y1, (x2 - x1) / 2);
+   } else if (type === "eclipse") {
+      roughElement = generator.ellipse(x1, y1, (x2 - x1) / 2, (y2 - y1) / 2);
+   }
+
+   return { id, x1, y1, x2, y2, type, roughElement };
+}
+
+function isWithinElement(x, y, element) {
+   const { type, x1, x2, y1, y2 } = element;
+   if (type === "rectangle") {
+      const minX = Math.min(x1, x2);
+      const maxX = Math.max(x1, x2);
+      const minY = Math.min(y1, y2);
+      const maxY = Math.max(y1, y2);
+      return x >= minX && x <= maxX && y >= minY && y <= maxY;
+   } else if (type === "line") {
+      const a = { x1, y1 };
+      const b = { x2, y2 };
+      const c = { x, y };
+      const offset = distance(a, b) - distance(a, c) + distance(b, c);
+      return Math.abs(offset) < 1;
+   } else if (type === "circle") {
+      const a = { x1, y1 };
+      const b = { x2, y2 };
+      const c = { x, y };
+      const offset = distance(a, b) - distance(a, c) + distance(b, c);
+      return Math.abs(offset) < 1;
+   } else if (type === "eclipse") {
+      const a = { x1, y1 };
+      const b = { x2, y2 };
+      const c = { x, y };
+      const offset = distance(a, b) - distance(a, c) + distance(b, c);
+      return Math.abs(offset) < 1;
+   }
+}
+
+const distance = (a, b) => {
+   Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+};
+
+function getElementAtPosition(x, y, element) {
+   return element.find((ele) => isWithinElement(x, y, ele));
 }
 
 const App = () => {
    const [element, setElement] = useState([]);
-   const [drawing, setDrawing] = useState(false);
-   const [elementType, setElementType] = useState("line");
+   const [action, setAction] = useState("none");
+   const [tool, setTool] = useState("line");
+   const [selectedElement, setSelectedElement] = useState(null);
 
    useLayoutEffect(() => {
       const canvas = document.getElementById("canvas");
@@ -26,48 +71,70 @@ const App = () => {
       element.forEach((ele) => roughCanvas.draw(ele.roughElement));
    }, [element]);
 
+   const updateElement = (id, x1, y1, x2, y2, type) => {
+      const updateElement = createElement(id, x1, y1, x2, y2, type);
+
+      const elementCopy = [...element];
+      elementCopy[id] = updateElement;
+      setElement(elementCopy);
+   };
+
    // When mouse move Down
    const handleMouseDown = (e) => {
-      setDrawing(true);
-
       const { clientX, clientY } = e;
+      if (tool === "selection") {
+         const ele = getElementAtPosition(clientX, clientY, element);
+         if (ele) {
+            const offsetX = clientX - ele.x1;
+            const offsetY = clientY - ele.y1;
 
-      const element = createElement(
-         clientX,
-         clientY,
-         clientX,
-         clientY,
-         elementType
-      );
-      setElement((prevState) => [...prevState, element]);
+            setSelectedElement({ ...ele, offsetX, offsetY });
+            setAction("moving");
+         }
+      } else {
+         const id = element.length;
+         const ele = createElement(
+            id,
+            clientX,
+            clientY,
+            clientX,
+            clientY,
+            tool
+         );
+         setElement((prevState) => [...prevState, ele]);
+         setAction("drawing");
+      }
    };
 
    // When mouse Move
    const handleMouseMove = (e) => {
-      if (!drawing) {
-         return;
-      }
       const { clientX, clientY } = e;
 
-      const index = element.length - 1;
-      const { x1, y1 } = element[index];
+      if (tool === "selection") {
+         e.target.style.cursor = getElementAtPosition(clientX, clientY, element)
+            ? "move"
+            : "default";
+      }
 
-      const updateElement = createElement(
-         x1,
-         y1,
-         clientX,
-         clientY,
-         elementType
-      );
+      if (action === "drawing") {
+         const index = element.length - 1;
+         const { x1, y1 } = element[index];
 
-      const elementCopy = [...element];
-      elementCopy[index] = updateElement;
-      setElement(elementCopy);
+         updateElement(index, x1, y1, clientX, clientY, tool);
+      } else if (action === "moving") {
+         const { id, x1, y1, x2, y2, type, offsetX, offsetY } = selectedElement;
+         const width = x2 - x1;
+         const height = y2 - y1;
+         const newX1 = clientX - offsetX;
+         const newY1 = clientY - offsetY;
+         updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type);
+      }
    };
 
    // When mouse move UP
    const handleMouseUp = () => {
-      setDrawing(false);
+      setAction("none");
+      setSelectedElement(null);
    };
 
    return (
@@ -75,18 +142,39 @@ const App = () => {
          <div style={{ position: "fixed" }}>
             <input
                type="radio"
+               id="selection"
+               checked={tool === "selection"}
+               onChange={() => setTool("selection")}
+            />
+            <label htmlFor="selection">Selection</label>
+            <input
+               type="radio"
                id="line"
-               checked={elementType === "line"}
-               onChange={() => setElementType("line")}
+               checked={tool === "line"}
+               onChange={() => setTool("line")}
             />
             <label htmlFor="line">Line</label>
             <input
                type="radio"
                id="rectangle"
-               checked={elementType === "rectangle"}
-               onChange={() => setElementType("rectangle")}
+               checked={tool === "rectangle"}
+               onChange={() => setTool("rectangle")}
             />
             <label htmlFor="rectangle">Rectangle</label>
+            <input
+               type="radio"
+               id="circle"
+               checked={tool === "circle"}
+               onChange={() => setTool("circle")}
+            />
+            <label htmlFor="rectangle">Circle</label>
+            <input
+               type="radio"
+               id="eclipse"
+               checked={tool === "eclipse"}
+               onChange={() => setTool("eclipse")}
+            />
+            <label htmlFor="rectangle">Eclipse</label>
          </div>
          <canvas
             id="canvas"
